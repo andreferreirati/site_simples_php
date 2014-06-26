713edcf3132bbf076e678c98dbaf9fcc66f72951
Luis Alberto Concha Curay <luvett11@gmail.com>
2014-06-26 17:26:40 -0300

    Adicionando classe para verificação de URL

---
+++ classes/Url.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * File: Url.php
+ * Author: Luis Alberto Concha Curay
+ * E-mail: luvett11@gmail.com
+ * Language: 
+ * Date: 26/06/14
+ * Time: 16:51
+ * Project: estudo_php
+ * Copyright: 2014
+ */
+
+namespace classes;
+
+
+class Url
+{
+
+    public static function verificaUrl( $pagina )
+    {
+        if( isset( $pagina ) ) {
+            if( file_exists( 'inc/' .$pagina.'.php' ) ) {
+               require_once( 'inc/' .$pagina.'.php' );
+            }else {
+                require_once( 'inc/error404.php' );
+            }
+        } else {
+            require_once( 'inc/error404.php' );
+        }
+    }
+} 
