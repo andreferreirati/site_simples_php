dafdef11f126fea79938ae79c912f997c8c1f8ee
Luis Alberto Concha Curay <luvett11@gmail.com>
2014-07-05 18:45:02 -0300

    Criado o arquivo de funções para auxilar no sistema, sendo a primeira paginaRequisitada()

---
+++ funcoes/funcoes.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * File: funcoes.php
+ * Author: Luis Alberto Concha Curay
+ * E-mail: luvett11@gmail.com
+ * Language: 
+ * Date: 05/07/14
+ * Time: 17:50
+ * Project: estudo_php
+ * Copyright: 2014
+ */
+
+/**
+ * Função que extrai o nome da pagina solicitada
+ * @return mixed
+ */
+function paginaRequisitada()
+{
+    $rota      = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+    $arrayRota = explode('/', $rota['path']);
+    $pagina    = $arrayRota[1];
+    return $pagina;
+}
