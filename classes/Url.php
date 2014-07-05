52b33aedd34fc9d53896d46f201ca3cebbcc930b
Luis Alberto Concha Curay <luvett11@gmail.com>
2014-07-05 18:50:34 -0300

    Alteração da função verificaUrl()

--- classes/Url.php
+++ classes/Url.php
@@ -16,11 +16,26 @@ namespace classes;
 class Url
 {
 
+    /** /
     public static function verificaUrl( $pagina )
     {
         if( isset( $pagina ) ) {
             if( file_exists( 'inc/' .$pagina.'.php' ) ) {
                require_once( 'inc/' .$pagina.'.php' );
+            }else {
+                require_once( 'inc/error404.php' );
+            }
+        } else {
+            require_once( 'inc/error404.php' );
+        }
+    }
+    /**/
+    
+    public static function verificaUrl( $pagina )
+    {
+        if( isset( $pagina ) ) {
+            if( file_exists( 'inc/' .$pagina.'.php' ) ) {
+                require_once( 'inc/' .$pagina.'.php' );
             }else {
                 require_once( 'inc/error404.php' );
             }
