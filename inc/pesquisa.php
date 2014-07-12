ed592b52ed12a41d48013e89407daab44807166a
Luis Alberto Concha Curay <luvett11@gmail.com>
2014-07-12 12:00:18 -0300

    1)Ajustes na string sql para buscar dados com acentuação. 2)Mostrar mensagem quando não achar resultados na BD

--- inc/pesquisa.php
+++ inc/pesquisa.php
@@ -16,16 +16,22 @@ $stringPesquisa = filter_input(INPUT_POST,'txtPesquisa', FILTER_SANITIZE_STRING)
 if( isset($stringPesquisa) AND $stringPesquisa <> null ) {
 
     $pdo = conecta();
-    $sql = "SELECT * FROM tbl_conteudo WHERE conteudo_conteudo LIKE '%$stringPesquisa%'";
+    $sql = "SELECT * FROM tbl_conteudo WHERE conteudo_conteudo LIKE _utf8 '%$stringPesquisa%' COLLATE utf8_unicode_ci";
     $stmt = $pdo->prepare( $sql );
     $stmt->execute();
     $noticiaEncontrada = $stmt->fetchAll( PDO::FETCH_OBJ );
-      echo '<ul>';
-      foreach( $noticiaEncontrada as $noticia) :
-        echo '<li><b>' .utf8_encode( $noticia->titulo_conteudo ).  '</b><br />';
-        echo limitarCaracter( $noticia->conteudo_conteudo, 500 ). '<a href="/'.$noticia->slug_conteudo .'">Leia mais</a>  </li>';
-      endforeach;
-      echo '</ul>';
+
+    if( !empty( $noticiaEncontrada ) ) {
+        echo '<ul>';
+        foreach( $noticiaEncontrada as $noticia) :
+            echo '<li><b>' .utf8_encode( $noticia->titulo_conteudo ).  '</b><br />';
+            echo limitarCaracter( $noticia->conteudo_conteudo, 500 ). '<a href="/'.$noticia->slug_conteudo .'">Leia mais</a>  </li>';
+        endforeach;
+        echo '</ul>';
+    }else{
+        echo '<p class="erroPesquisa">Nenhum dado encontrado. Palavra utilizada na pesquisa: <b>"'.$stringPesquisa.'"</b></p>';
+    }
+
 }else{
     echo '<p class="erroPesquisa">Favor informar a palavra a ser pesquisada</p>';
 }
