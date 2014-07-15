f8c387f2661f5f8a18dcd552b5173debba4ff4f0
Luis Alberto Concha Curay <luvett11@gmail.com>
2014-07-14 21:15:47 -0300

    Ocultando menus caso não exista estrutura sql suficiente para roda a aplicação

--- inc/nav_bar.php
+++ inc/nav_bar.php
@@ -56,8 +56,39 @@ try {
 
         $stmt->execute();
         $menu = $stmt->fetchAll( PDO::FETCH_ASSOC);
-
         $pdo = null;
+?>
+
+
+        <!-- Fixed navbar -->
+        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/">Site Simples em php</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <?php foreach( $menu as $m ) : ?>
+                            <li><a href="<?php echo $m['href_menu'] ?>" title="<?php echo utf8_encode( $m['hint_menu'] );?>"><?php echo utf8_encode( $m['nome_menu'] ); ?></a></li>
+                        <?php endforeach; ?>
+
+                    </ul>
+                    <form class="navbar-form navbar-right" method="post" action="pesquisa">
+                        <input type="text" placeholder="Pesquisar" class="form-control" id="txtPesquisa" name="txtPesquisa">
+                        <input type="submit" value="Pesquisar.."/>
+                    </form>
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
+
+<?php
+
     }catch (PDOException $e){
         echo '<h2 class="fixturError">Prezado Usuário:</h2>';
         echo '<p class="fixturErrorMsgUsuario">Antes de rodre á aplicação é necessário executar o script2.sql para montar a estrutura do site
@@ -70,30 +101,3 @@ try {
 
 
 ?>
-
-<!-- Fixed navbar -->
-<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/">Site Simples em php</a>
-        </div>
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <?php foreach( $menu as $m ) : ?>
-                    <li><a href="<?php echo $m['href_menu'] ?>" title="<?php echo utf8_encode( $m['hint_menu'] );?>"><?php echo utf8_encode( $m['nome_menu'] ); ?></a></li>
-                <?php endforeach; ?>
-
-            </ul>
-            <form class="navbar-form navbar-right" method="post" action="pesquisa">
-                <input type="text" placeholder="Pesquisar" class="form-control" id="txtPesquisa" name="txtPesquisa">
-                <input type="submit" value="Pesquisar.."/>
-            </form>
-        </div><!--/.nav-collapse -->
-    </div>
-</div>
