ca49abe1a0eea7bf1a894806cb5f68bcee6634ea
Luis Alberto Concha Curay <luvett11@gmail.com>
2014-07-22 23:17:21 -0300

    Deletando arquivos desnecessários

--- sql/script.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `tbl_clientes` (`id_cliente` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,`nome_cliente` varchar(100) NOT NULL,`email_cliente` varchar(50) NOT NULL) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
-INSERT INTO `tbl_clientes`(`nome_cliente`, `email_cliente`) VALUES ('Cliente 01', 'cliente01@gmail.com');
-INSERT INTO `tbl_clientes`(`nome_cliente`, `email_cliente`) VALUES ('Cliente 02', 'cliente02@gmail.com');
-INSERT INTO `tbl_clientes`(`nome_cliente`, `email_cliente`) VALUES ('Cliente 03', 'cliente03@gmail.com');
-INSERT INTO `tbl_clientes`(`nome_cliente`, `email_cliente`) VALUES ('Cliente 04', 'cliente04@gmail.com');
-INSERT INTO `tbl_clientes`(`nome_cliente`, `email_cliente`) VALUES ('Cliente 01', 'cliente01@gmail.com');
-INSERT INTO `tbl_clientes`(`nome_cliente`, `email_cliente`) VALUES ('Cliente 02', 'cliente02@gmail.com');
-INSERT INTO `tbl_clientes`(`nome_cliente`, `email_cliente`) VALUES ('Cliente 03', 'cliente03@gmail.com');
-INSERT INTO `tbl_clientes`(`nome_cliente`, `email_cliente`) VALUES ('Cliente 04', 'cliente04@gmail.com');
